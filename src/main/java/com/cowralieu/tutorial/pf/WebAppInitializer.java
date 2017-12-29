094724e6cf4c42bf08e40caa3dd9e7b2a9ada15a
Liu Qian <cowragg@outlook.com>
2017-12-30 01:55:36 +0800

    integrate with spring

---
+++ src/main/java/com/cowralieu/tutorial/pf/WebAppInitializer.java
@@ -0,0 +1,20 @@
+package com.cowralieu.tutorial.pf;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(SpringConfiguration.class);
+
+        // Manage the lifecycle of the root application context
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+    }
+}
