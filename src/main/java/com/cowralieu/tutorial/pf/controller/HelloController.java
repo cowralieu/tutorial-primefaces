c641fca14ca90308de36fb22d5e2a2b9dfe4d906
Liu Qian <cowragg@outlook.com>
2017-12-30 18:10:24 +0800

    equivalent DI by spring

--- src/main/java/com/cowralieu/tutorial/pf/controller/HelloController.java
+++ src/main/java/com/cowralieu/tutorial/pf/controller/HelloController.java
@@ -1,21 +1,31 @@
 package com.cowralieu.tutorial.pf.controller;
 
 import com.cowralieu.tutorial.pf.service.HelloSpringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 
-@ManagedBean
+//@ManagedBean
+@Scope("request")
+@Component
 public class HelloController {
 
-    @ManagedProperty("#{hss}")
+//    @ManagedProperty("#{hss}")
+    @Autowired
+    @Qualifier("hss")
     private HelloSpringService helloSpringService;
 
     public String showHello() {
         return helloSpringService.sayHello();
     }
 
+    /*
     public void setHelloSpringService(HelloSpringService helloSpringService) {
         this.helloSpringService = helloSpringService;
     }
+    */
 }
